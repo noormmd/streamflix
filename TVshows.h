a84d31c2ed9cf2b54b6bda27f0307b83238c0816
noormmd <116109904+noormmd@users.noreply.github.com>
2024-03-19 00:28:38 +0000

    Added method and constructor definitions for TVshows class

--- TVshows.h
+++ TVshows.h
@@ -21,6 +21,28 @@ class TVshows {
     vector<string> genre;
 
     public:
-}
+    // Constructor
+    TVshows(int duration, int id, const string& name, const string& genre, const string& first, const string& last,
+            int seasons, const vector<int>& episodes, const vector<string>& genreList)
+        : episodeDuration(duration), showID(id), showName(name), showGenre(genre), actorFirst(first),
+          actorLast(last), seasonsCount(seasons), episodeCount(episodes), genre(genreList) {}
+    
+    int getSeasonCount() const {
+        return seasonCount;
+    }
+
+    int getEpisodeCount() const{
+        return episodeCount.size(); //We want to return the total number of episodes
+    }
+
+    void selectEpisode(int seasonNumber, int episodeNumber) {}
+
+    void selectSeason(int seasonNumber) {}
+
+    void selectShow() {}
+
+    void watchShowLater() {}
+    
+};
 
 #endif
