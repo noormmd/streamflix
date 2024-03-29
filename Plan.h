0f19183ce33771f04480795f5295b35786f1ff1b
noormmd <noormamdouh004@gmail.com>
2024-03-29 22:13:17 +0000

    Debugging, removing duplication of classes and correcting attributes

--- Plan.h
+++ Plan.h
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-
-
 class Plan {
 private:
     float price;
--- TVshows.h
+++ TVshows.h
@@ -25,14 +25,14 @@ class TVshows {
     TVshows(int duration, int id, const string& name, const string& genre, const string& first, const string& last,
             int seasons, const vector<int>& episodes, const vector<string>& genreList)
         : episodeDuration(duration), showID(id), showName(name), showGenre(genre), actorFirst(first),
-          actorLast(last), seasonsCount(seasons), episodeCount(episodes), genre(genreList) {}
+          actorLast(last), seasonsCount(seasons), episodesCount(episodes), genre(genreList) {}
     
     int getSeasonCount() const {
-        return seasonCount;
+        return seasonsCount;
     }
 
     int getEpisodeCount() const{
-        return episodeCount.size(); //We want to return the total number of episodes
+        return episodesCount.size(); //We want to return the total number of episodes
     }
 
     void selectEpisode(int seasonNumber, int episodeNumber) {}
